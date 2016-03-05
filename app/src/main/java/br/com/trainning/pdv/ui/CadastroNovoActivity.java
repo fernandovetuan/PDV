ad3c663d19a1711562ac0be2d3948c4580e4a62f
fernandovetuan <fernando.vertuan@gmail.com>
2016-03-05 12:30:10 -0300

    Atualizacao ate 10:41

--- app/src/main/java/br/com/trainning/pdv/ui/CadastroNovoActivity.java
+++ app/src/main/java/br/com/trainning/pdv/ui/CadastroNovoActivity.java
@@ -5,11 +5,23 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import br.com.trainning.pdv.R;
+import butterknife.Bind;
 
-public class CadastroNovoActivity extends AppCompatActivity {
+public class CadastroNovoActivity extends BasicActivity {
+
+    @Bind(R.id.txtDescricao)
+    EditText txtDescricao;
+    @Bind(R.id.txtUnidade)
+    EditText txtUnidade;
+    @Bind(R.id.txtPreco)
+    EditText txtPreco;
+    @Bind(R.id.txtCodigoBarras)
+    EditText txtCodigoBarras;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +30,22 @@ public class CadastroNovoActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        /*txtDescricao = (EditText)findViewById(R.id.txtDescricao);
+        txtUnidade = (EditText)findViewById(R.id.txtUnidade);
+        txtPreco = (EditText)findViewById(R.id.txtPreco);
+        txtCodigoBarras =  (EditText)findViewById(R.id.txtCodigoBarras);*/
+
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Log.d("Cadastro",txtDescricao.getText().toString());
+                Log.d("Unidade",txtUnidade.getText().toString());
+                Log.d("Preco",txtPreco.getText().toString());
+                Log.d("Codigo",txtCodigoBarras.getText().toString());
+
             }
         });
     }
