c3ea8dd04ba6566c3fa179551498bde584e6cff2
fernandovetuan <fernando.vertuan@gmail.com>
2016-03-12 15:55:44 -0300

    Sabado 12/03 15:55

--- app/src/main/java/br/com/trainning/pdv/ui/MainActivity.java
+++ app/src/main/java/br/com/trainning/pdv/ui/MainActivity.java
@@ -2,6 +2,8 @@ package br.com.trainning.pdv.ui;
 
 import android.app.DownloadManager;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,14 +13,38 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.trainning.pdv.R;
+import br.com.trainning.pdv.domain.adapter.CustomArrayAdapter;
+import br.com.trainning.pdv.domain.model.Item;
+import br.com.trainning.pdv.domain.model.ItemProduto;
 import br.com.trainning.pdv.domain.model.Produto;
+import butterknife.Bind;
+import jim.h.common.android.lib.zxing.config.ZXingLibConfig;
+import jim.h.common.android.lib.zxing.integrator.IntentIntegrator;
+import jim.h.common.android.lib.zxing.integrator.IntentResult;
+import se.emilsjolander.sprinkles.CursorList;
 import se.emilsjolander.sprinkles.Query;
 
 public class MainActivity extends BasicActivity {
+
+    private ZXingLibConfig zxingLibConfig;
+    @Bind(R.id.listView)
+    SwipeMenuListView listView;
+
+    private List<ItemProduto> list;
+    private int quantidadeItens;
+    private double valorTotal;
+    private CustomArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +53,72 @@ public class MainActivity extends BasicActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        zxingLibConfig = new ZXingLibConfig();
+        zxingLibConfig.useFrontLight = true;
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                IntentIntegrator.initiateScan(MainActivity.this, zxingLibConfig);
             }
         });
 
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+            @Override
+            public void create(SwipeMenu menu) {
+                // create "open" item
+                SwipeMenuItem openItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                openItem.setBackground(new ColorDrawable(Color.rgb(0xC9, 0xC9,
+                        0xCE)));
+                // set item width
+                openItem.setWidth(90);
+                // set item title
+                openItem.setTitle("Open");
+                // set item title fontsize
+                openItem.setTitleSize(18);
+                // set item title font color
+                openItem.setTitleColor(Color.WHITE);
+                // add to menu
+                menu.addMenuItem(openItem);
+
+                // create "delete" item
+                SwipeMenuItem deleteItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
+                        0x3F, 0x25)));
+                // set item width
+                deleteItem.setWidth(90);
+                // set a icon
+                deleteItem.setIcon(R.drawable.ic_remove_shopping_cart_white_36dp);
+                // add to menu
+                menu.addMenuItem(deleteItem);
+            }
+        };
+
+        // set creator
+        listView.setMenuCreator(creator);
+
+        listView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                switch (index) {
+                    case 0:
+                        // open
+                        break;
+                    case 1:
+                        // delete
+                        break;
+                }
+                // false : close the menu; true : not close the menu
+                return false;
+            }
+        });
 
     }
 
@@ -84,4 +167,72 @@ public class MainActivity extends BasicActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case IntentIntegrator.REQUEST_CODE:
+
+                IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode,
+                        resultCode, data);
+                if (scanResult == null) {
+                    return;
+                }
+                String result = scanResult.getContents();
+                if (result != null) {
+                    Log.d("Scan Bar Code : ",result);
+
+                    Produto produto = Query.one(Produto.class, "select * from produto where codigo_barra = ?", result).get();
+
+                    if (produto!=null)
+                    {
+                        Item item = new Item();
+                        item.setId(0L);
+                        item.setIdCompra(1L);
+                        item.setIdProduto(produto.getCodigoBarras());
+                        item.setQuantidade(1);
+                        item.save();
+                    }
+                    else
+                    {
+                        Toast.makeText(MainActivity.this, "Produto não localizado...", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+
+            default:
+        }
+
+    }
+
+    public void popularLista(){
+        List<Item> listaItem = Query.many(Item.class, "select * from item where id_compra = ? order by id", 1).get().asList();
+        Log.d("TAMANHOLISTA",""+ listaItem.size());
+
+        ItemProduto itemProduto;
+        Produto produto;
+        list = new ArrayList<>();
+        valorTotal=0.0d;
+        quantidadeItens = 0;
+
+        for(Item item:listaItem){
+
+            produto = Query.one(Produto.class,"select * from produto where codigo_barra = ?", item.getIdProduto()).get();
+            itemProduto = new ItemProduto();
+            itemProduto.setIdCompra(1);
+            itemProduto.setIdItem(item.getId());
+            itemProduto.setFoto(produto.getFoto());
+            itemProduto.setDescricao(produto.getDescricao());
+            itemProduto.setQuantidade(item.getQuantidade());
+            itemProduto.setPreco(produto.getPreco());
+            list.add(itemProduto);
+            valorTotal+=item.getQuantidade()*produto.getPreco();
+            quantidadeItens += item.getQuantidade();
+        }
+        getSupportActionBar().setTitle("PDV " + valorTotal);
+        adapter = new CustomArrayAdapter(this, R.layout.list_item, list);
+        listView.setAdapter(adapter);
+    }
+
 }
