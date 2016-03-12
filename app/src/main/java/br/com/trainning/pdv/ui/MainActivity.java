fce0014c1b83844f8d98accdd221528ae11449ff
fernandovetuan <fernando.vertuan@gmail.com>
2016-03-12 17:50:51 -0300

    Sabado 12/03 15:55

--- app/src/main/java/br/com/trainning/pdv/ui/MainActivity.java
+++ app/src/main/java/br/com/trainning/pdv/ui/MainActivity.java
@@ -79,11 +79,7 @@ public class MainActivity extends BasicActivity {
                 // set item width
                 openItem.setWidth(Util.convertPixelsToDp(290,getApplicationContext()));
                 // set item title
-                openItem.setTitle("Open");
-                // set item title fontsize
-                openItem.setTitleSize(18);
-                // set item title font color
-                openItem.setTitleColor(Color.WHITE);
+                openItem.setIcon(R.drawable.ic_exposure_plus_1_black_36dp);
                 // add to menu
                 menu.addMenuItem(openItem);
 
@@ -106,20 +102,31 @@ public class MainActivity extends BasicActivity {
         listView.setMenuCreator(creator);
 
         listView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+
             @Override
             public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                ItemProduto itemProduto = adapter.getItem(position);
+                Item item = Query.one(Item.class,"select * from item where id = ?", itemProduto.getIdItem()).get();
                 switch (index) {
                     case 0:
-                        // open
+                        //Toast.makeText(getApplicationContext(), "Action 1 for " + itemProduto.getDescricao(), Toast.LENGTH_SHORT).show();
+                        item.setQuantidade(item.getQuantidade()+1);
+                        item.save();
+                        list.clear();
+                        popularLista();
                         break;
                     case 1:
-                        // delete
+                        //Toast.makeText(getApplicationContext(), "Action 2 for " + itemProduto.getDescricao(), Toast.LENGTH_SHORT).show();
+                        item.delete();
+                        list.clear();
+                        popularLista();
+
                         break;
                 }
-                // false : close the menu; true : not close the menu
                 return false;
             }
         });
+
 
         popularLista();
 
@@ -221,7 +228,7 @@ public class MainActivity extends BasicActivity {
         valorTotal=0.0d;
         quantidadeItens = 0;
 
-        produto = Query.one(Produto.class,"select * from produto where codigo_barra = ?", 10).get();
+       /* produto = Query.one(Produto.class,"select * from produto where codigo_barra = ?", 10).get();
         itemProduto = new ItemProduto();
         itemProduto.setIdCompra(1);
         itemProduto.setIdItem(1);
@@ -232,7 +239,7 @@ public class MainActivity extends BasicActivity {
         itemProduto.setPreco(produto.getPreco());
         list.add(itemProduto);
         valorTotal+=produto.getPreco();
-        quantidadeItens += 1;
+        quantidadeItens += 1;*/
 
         for(Item item:listaItem){
 
